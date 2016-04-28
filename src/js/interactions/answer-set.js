a68503a7c4432c8d0e60fa9ac2aa19cb86fb5d6e
Jer <thejhereg@gmail.com>
2016-04-28 07:46:20 -0500

    aria updated
    Another update — atomic was pointless on an element with aria-live
    turned off. Removed init entirely.

--- src/js/interactions/answer-set.js
+++ src/js/interactions/answer-set.js
@@ -4,143 +4,142 @@
  * Licensed GPL (https://github.com/aut0poietic/ali/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
-(function ($) {
-    "use strict";
+(function ( $ ) {
+	"use strict";
 
-    // Make the global object available and abort if this file is used without it.
-    var ali = window.ali;
-    if ($.type(ali) !== 'object') {
-        return;
-    }
+	// Make the global object available and abort if this file is used without it.
+	var ali = window.ali;
+	if ( $.type( ali ) !== 'object' ) {
+		return;
+	}
 
-    var DESCRIPTION = 'Answer Set multi-part interaction.';
-    var TYPE = ali.TYPE.choice;
+	var DESCRIPTION = 'Answer Set multi-part interaction.';
+	var TYPE = ali.TYPE.choice;
 
-    var QUESTION = 'form';
-    var SELECT = 'select';
-    var SUBMIT = 'button';
-    var CORRECT_ATTR = 'data-ali-correct';
-    var CORRECT_RESPONSE = 'data-ali-correct-message';
-    var INCORRECT_RESPONSE = 'data-ali-incorrect-message';
+	var QUESTION = 'form';
+	var SELECT = 'select';
+	var SUBMIT = 'button';
+	var CORRECT_ATTR = 'data-ali-correct';
+	var CORRECT_RESPONSE = 'data-ali-correct-message';
+	var INCORRECT_RESPONSE = 'data-ali-incorrect-message';
 
-    /**
-     * Answer Set Interaction
-     * @param element DOMElement
-     * @constructor
-     */
-    ali.AnswerSet = function (element) {
-        ali.Interaction.call(this, element, TYPE, DESCRIPTION);
-        this.init();
-    };
+	/**
+	 * Answer Set Interaction
+	 * @param element DOMElement
+	 * @constructor
+	 */
+	ali.AnswerSet = function ( element ) {
+		ali.Interaction.call( this, element, TYPE, DESCRIPTION );
+		this.init();
+	};
 
-    // Inherits from ali.Interaction
-    ali.AnswerSet.prototype = Object.create(ali.Interaction.prototype);
-    ali.AnswerSet.prototype.constructor = ali.AnswerSet;
+	// Inherits from ali.Interaction
+	ali.AnswerSet.prototype = Object.create( ali.Interaction.prototype );
+	ali.AnswerSet.prototype.constructor = ali.AnswerSet;
 
-    /**
-     * Initialization.
-     */
-    ali.AnswerSet.prototype.init = function () {
-        $(QUESTION, this.$el).each((function (i, el) {
-            $(el).aria({
-                           'live' : "off",
-                           'atomic' : "true"
-                       })
-                .attr('id', this.$el.attr('id') + '-' + i)
-                .off('submit.ali')
-                .on('submit.ali', this.question_onSubmit.bind(this));
-        }).bind(this));
+	/**
+	 * Initialization.
+	 */
+	ali.AnswerSet.prototype.init = function () {
+		$( QUESTION, this.$el ).each( (function ( i, el ) {
+			$( el ).attr( 'id', this.$el.attr( 'id' ) + '-' + i )
+			       .off( 'submit.ali' )
+			       .on( 'submit.ali', this.question_onSubmit.bind( this ) );
+		}).bind( this ) );
 
-        $(QUESTION + ' ' + SELECT, this.$el)
-            .off('change.ali')
-            .on('change.ali', (function (e) {
-                var $form = $($(e.target).parents(QUESTION)[0]);
-                this.itemSelected($form);
-            }).bind(this));
-    };
+		$( QUESTION + ' ' + SELECT, this.$el )
+			.off( 'change.ali' )
+			.on( 'change.ali', (function ( e ) {
+				var $form = $( $( e.target ).parents( QUESTION )[ 0 ] );
+				this.itemSelected( $form );
+			}).bind( this ) );
+	};
 
-    /**
-     * Event handler for form submit for each question.
-     * @param e
-     */
-    ali.AnswerSet.prototype.question_onSubmit = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        var $target = $(e.target);
-        var is_correct = parseInt($(SELECT, $target).prop('selectedIndex')) === parseInt($target.attr(CORRECT_ATTR), 10);
-        this.showFlag($target, is_correct);
-        this.disableQuestion($target);
+	/**
+	 * Event handler for form submit for each question.
+	 * @param e
+	 */
+	ali.AnswerSet.prototype.question_onSubmit = function ( e ) {
+		e.preventDefault();
+		e.stopPropagation();
+		var $target = $( e.target );
+		var is_correct = parseInt( $( SELECT, $target ).prop( 'selectedIndex' ) ) === parseInt( $target.attr( CORRECT_ATTR ), 10 );
+		this.showFlag( $target, is_correct );
+		this.disableQuestion( $target );
 
-        this.setCorrectResponses([$(SELECT + ' option', $target).eq(parseInt($target.attr(CORRECT_ATTR), 10)).val()]);
-        this.setLearnerResponses([$(SELECT, $target).val()]);
-        this.itemComplete(is_correct ? ali.STATUS.correct : ali.STATUS.incorrect, $target);
-        this.allQuestionsComplete();
-    };
+		this.setCorrectResponses( [ $( SELECT + ' option', $target ).eq( parseInt( $target.attr( CORRECT_ATTR ), 10 ) ).val() ] );
+		this.setLearnerResponses( [ $( SELECT, $target ).val() ] );
+		this.itemComplete( is_correct ? ali.STATUS.correct : ali.STATUS.incorrect, $target );
+		this.allQuestionsComplete();
+	};
 
-    /**
-     *
-     */
-    ali.AnswerSet.prototype.allQuestionsComplete = function () {
-        var num_questions = $(QUESTION).length, is_correct;
-        if (num_questions === $(QUESTION + '[aria-disabled="true"]').length) {
-            is_correct = num_questions === $(QUESTION + ' .correct').length;
-            this.complete(is_correct ? ali.STATUS.correct : ali.STATUS.incorrect);
-        }
-    };
+	/**
+	 *
+	 */
+	ali.AnswerSet.prototype.allQuestionsComplete = function () {
+		var num_questions = $( QUESTION ).length, is_correct;
+		if ( num_questions === $( QUESTION + '[aria-disabled="true"]' ).length ) {
+			is_correct = num_questions === $( QUESTION + ' .correct' ).length;
+			this.complete( is_correct ? ali.STATUS.correct : ali.STATUS.incorrect );
+		}
+	};
 
-    /**
-     *
-     * @param $target
-     */
-    ali.AnswerSet.prototype.disableQuestion = function ($target) {
-        $target.aria('disabled', 'true');
-        $(SELECT + ',' + SUBMIT, $target).aria('disabled', 'true');
-    };
+	/**
+	 *
+	 * @param $target
+	 */
+	ali.AnswerSet.prototype.disableQuestion = function ( $target ) {
+		$target.aria( 'disabled', 'true' );
+		$( SELECT + ',' + SUBMIT, $target ).aria( 'disabled', 'true' );
+	};
 
-    /**
-     *
-     * @param $target
-     * @param is_correct
-     */
-    ali.AnswerSet.prototype.showFlag = function ($target, is_correct) {
-        $target.aria('live', 'assertive');
-        var $flag = this.makeFlag(
-            is_correct ? $target.attr(CORRECT_RESPONSE) : $target.attr(INCORRECT_RESPONSE),
-            is_correct ? 'correct' : 'incorrect');
+	/**
+	 *
+	 * @param $target
+	 * @param is_correct
+	 */
+	ali.AnswerSet.prototype.showFlag = function ( $target, is_correct ) {
+		$target.aria( {
+			              'live'   : "assertive",
+			              'atomic' : "true"
+		              } );
+		var $flag = this.makeFlag(
+			is_correct ? $target.attr( CORRECT_RESPONSE ) : $target.attr( INCORRECT_RESPONSE ),
+			is_correct ? 'correct' : 'incorrect' );
 
-        $flag.aria('hidden', 'true')
-            .appendTo($target);
+		$flag.aria( 'hidden', 'true' )
+		     .appendTo( $target );
 
-        setTimeout((function () {
-            $flag.aria('hidden', 'false');
-        }).bind(this), 1);
-    };
+		setTimeout( (function () {
+			$flag.aria( 'hidden', 'false' );
+		}).bind( this ), 1 );
+	};
 
-    /**
-     *
-     * @param msg
-     * @param cls
-     */
-    ali.AnswerSet.prototype.makeFlag = function (msg, cls) {
-        return $('<div class="flag ' + cls + '"><span>' + msg + '</span></div>');
-    };
+	/**
+	 *
+	 * @param msg
+	 * @param cls
+	 */
+	ali.AnswerSet.prototype.makeFlag = function ( msg, cls ) {
+		return $( '<div class="flag ' + cls + '"><span>' + msg + '</span></div>' );
+	};
 
-    /*
-     * jQuery Plugin
-     */
-    function Plugin() {
-        return this.each(function () {
-            new ali.AnswerSet(this);
-        });
-    }
+	/*
+	 * jQuery Plugin
+	 */
+	function Plugin() {
+		return this.each( function () {
+			new ali.AnswerSet( this );
+		} );
+	}
 
-    var old = $.fn.answerset;
-    $.fn.answerset = Plugin;
-    $.fn.answerset.Constructor = ali.AnswerSet;
+	var old = $.fn.answerset;
+	$.fn.answerset = Plugin;
+	$.fn.answerset.Constructor = ali.AnswerSet;
 
-    $.fn.answerset.noConflict = function () {
-        $.fn.answerset = old;
-        return this;
-    };
+	$.fn.answerset.noConflict = function () {
+		$.fn.answerset = old;
+		return this;
+	};
 
-})(jQuery);
+})( jQuery );
